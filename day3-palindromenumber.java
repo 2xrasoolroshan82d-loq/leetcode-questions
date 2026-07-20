2d3523b2b7b726e0bf3ed7a116f675e04e93b783
2xrasoolroshan82d-loq <2xrasoolroshan82d@gmail.com>
2026-07-20 09:40:23 +0530

    Create day1-palindromenumber.java

---
+++ day3-palindromenumber.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x < 0) return false;
+        int num = 0;
+        int og = x;
+        while(x > 0)
+        {
+            int rem = x % 10;
+            num = (num * 10) + rem;
+            x /= 10;
+        }
+        if(og == num) return true;
+        return false;
+    }
+}
