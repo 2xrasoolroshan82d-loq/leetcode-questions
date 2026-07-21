9a04fda3e4f2a441fc52bfa491390434b3df7b3d
2xrasoolroshan82d-loq <2xrasoolroshan82d@gmail.com>
2026-07-21 10:59:07 +0530

    Create day-2-1603-designparkingsystem.java

---
+++ day-2-1603-designparkingsystem.java
@@ -0,0 +1,8 @@
+    private int[] count;
+    public ParkingSystem(int big, int medium, int small) {
+        count = new int[]{0, big, medium, small};
+    }
+    
+    public boolean addCar(int carType) {
+        return count[carType]-- > 0;
+    }
