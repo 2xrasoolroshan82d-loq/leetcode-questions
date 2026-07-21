3255ba543bc13459ae2f7fdbdb1d3c9945a12ca0
2xrasoolroshan82d-loq <2xrasoolroshan82d@gmail.com>
2026-07-21 11:13:44 +0530

    Create day-6-validpalindrome.java

---
+++ day-6-validpalindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length()-1;
+        while(left < right){
+
+            //skip space from the left
+            if(!Character.isLetterOrDigit(s.charAt(left))){
+                left++;
+                continue;
+            }
+            //skip space from the right
+            if(!Character.isLetterOrDigit(s.charAt(right))){
+                right--;
+                continue;
+            }
+
+            //compare both character in lowercase
+            if(Character.toLowerCase(s.charAt(left))!= Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
